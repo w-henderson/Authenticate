c52ef0436e852fcf73ce8f68920a993ca89a30ac
William Henderson <william-henderson@outlook.com>
2022-01-21 12:04:54 +0000

    Added option to not use logos (#3)

--- src/logos/logos.ts
+++ src/logos/logos.ts
@@ -1,19 +1,26 @@
 import images from "../images";
 
+const ENABLE_BRANDED_LOGOS = false;
+
 export function getLogo(issuer: string) {
-  switch (issuer.toLowerCase()) {
-    case "google": return images.logos.google;
-    case "microsoft": return images.logos.microsoft;
-    case "paypal": return images.logos.paypal;
-    case "discord": return images.logos.discord;
-    case "github": return images.logos.github;
-    case "instagram": return images.logos.instagram;
-    case "twitter": return images.logos.twitter;
-    case "amazon": return images.logos.amazon;
-    case "facebook": return images.logos.facebook;
-    case "reddit": return images.logos.reddit;
-    case "netflix": return images.logos.netflix;
-    case "twitch": return images.logos.twitch;
-    default: return images.logos.default;
+  if (ENABLE_BRANDED_LOGOS) {
+    switch (issuer.toLowerCase()) {
+      case "google": return images.logos.branded.google;
+      case "microsoft": return images.logos.branded.microsoft;
+      case "paypal": return images.logos.branded.paypal;
+      case "discord": return images.logos.branded.discord;
+      case "github": return images.logos.branded.github;
+      case "instagram": return images.logos.branded.instagram;
+      case "twitter": return images.logos.branded.twitter;
+      case "amazon": return images.logos.branded.amazon;
+      case "facebook": return images.logos.branded.facebook;
+      case "reddit": return images.logos.branded.reddit;
+      case "netflix": return images.logos.branded.netflix;
+      case "twitch": return images.logos.branded.twitch;
+      default: return images.logos.default;
+    }
+  } else {
+    let firstChar = issuer.toLowerCase().charAt(0);
+    return (images.logos.letters as any)[firstChar] || images.logos.default;
   }
 }
