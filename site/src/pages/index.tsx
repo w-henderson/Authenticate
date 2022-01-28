3da11b457903578927b94dfb5e5b257e238b228d
William Henderson <william-henderson@outlook.com>
2022-01-28 12:41:36 +0000

    Added animations to the site

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -3,8 +3,64 @@ import { Helmet } from "react-helmet";
 
 import "../styles/base.scss";
 
-class Index extends Component {
+interface ScrollAnimations {
+  heroAnimated: boolean,
+  drawerAnimated: boolean,
+  welcomeAnimated: boolean
+}
+
+class Index extends Component<{}, ScrollAnimations> {
+  drawerRef: React.RefObject<HTMLImageElement>;
+  welcomeRef: React.RefObject<HTMLImageElement>;
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      heroAnimated: true,
+      drawerAnimated: false,
+      welcomeAnimated: false
+    };
+
+    this.drawerRef = React.createRef();
+    this.welcomeRef = React.createRef();
+
+    this.handleScrollAnimation = this.handleScrollAnimation.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScrollAnimation);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScrollAnimation);
+  }
+
+  handleScrollAnimation() {
+    let y: number = window.scrollY;
+
+    let heroAnimated = true;
+    let drawerAnimated = y > this.drawerRef!.current!.y;
+    let welcomeAnimated = y > this.welcomeRef!.current!.y;
+
+    this.setState({
+      heroAnimated,
+      drawerAnimated,
+      welcomeAnimated
+    });
+  }
+
   render() {
+    let visible = {
+      animation: "animateIn 0.8s ease forwards",
+      transform: "translateY(50px)",
+      opacity: "0",
+    };
+
+    let hidden = {
+      opacity: "0",
+    };
+
     return (
       <main>
         <Helmet>
@@ -34,7 +90,33 @@ class Index extends Component {
           </div>
 
           <div className="image">
-            <img src="/static/images/mockup.png" alt="Screenshot" />
+            <img src="/static/images/mockup.png" alt="Screenshot" style={this.state.heroAnimated ? visible : hidden} />
+          </div>
+        </section>
+
+        <section>
+          <div className="text">
+            <h1><span>Unlimited accounts,</span> unlimited security.</h1>
+            <p>
+              Authenticate lets you secure as many accounts as you want, as well as keeping the most important ones at the top.
+            </p>
+          </div>
+
+          <div className="image">
+            <img src="/static/images/drawer.png" alt="Screenshot" ref={this.drawerRef} style={this.state.drawerAnimated ? visible : hidden} />
+          </div>
+        </section>
+
+        <section>
+          <div className="text">
+            <h1><span>Two-factor authentication</span> shouldn't be hard.</h1>
+            <p>
+              That's why Authenticate is built to be easy to use and easy to understand, without compromising on functionality or security.
+            </p>
+          </div>
+
+          <div className="image">
+            <img src="/static/images/welcome.png" alt="Screenshot" ref={this.welcomeRef} style={this.state.welcomeAnimated ? visible : hidden} />
           </div>
         </section>
       </main>
